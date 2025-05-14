5b8e825bf330451972f4ce0f3029bbd7e1898d2f
Erhabington <ehabqasim466@gmail.com>
2025-05-14 23:39:00 +0500

    heirs

--- WarasatMobile/data/HeirsData.ts
+++ WarasatMobile/data/HeirsData.ts
@@ -1,27 +1,10 @@
 export const common_heirs = [
-  { relation: "son", category: ["common", "children"], limit: 99 },
-  { relation: "daughter", category: ["common", "children"], limit: 99 },
-  { relation: "father", category: ["common", "parent"], limit: 1 },
-  { relation: "mother", category: ["common", "parent"], limit: 1 },
-  { relation: "husband", category: ["common", "spouse"], limit: 1 },
-  { relation: "wife", category: ["common", "spouse"], limit: 4 },
-  { relation: "son's son", category: ["common", "children"], limit: 99 },
-  { relation: "son's daughter", category: ["common", "children"], limit: 99 },
-  { relation: "father's father", category: ["common", "parent"], limit: 1 },
-  { relation: "father's mother", category: ["common", "parent"], limit: 1 },
-  { relation: "mother's mother", category: ["common", "parent"], limit: 1 },
-  { relation: "real brother", category: ["common", "sibling"], limit: 99 },
-  { relation: "real sister", category: ["common", "sibling"], limit: 99 },
-  { relation: "paternal brother", category: ["common", "sibling"], limit: 99 },
-  { relation: "paternal sister", category: ["common", "sibling"], limit: 99 },
-  {
-    relation: "maternal sibling",
-    category: ["common", "sibling"],
-    limit: 99,
-  },
-  { relation: "son's son's son", category: ["children"], limit: 99 },
-  { relation: "son's son's daughter", category: ["children"], limit: 99 },
-  { relation: "father's father's father", category: ["parent"], limit: 1 },
+    { relation: "son", val: 1, limit: 100 },
+    { relation: "daughter", val: 1, limit: 100 },
+    { relation: "father", val: 1, limit: 1 },
+    { relation: "mother", val: 1, limit: 1 },
+    { relation: "husband", val: 1, limit: 1 },
+    { relation: "wife", val: 1, limit: 4 },
   ]
   
   export const parent_heirs_grouped = [
